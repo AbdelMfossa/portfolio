3adefc845542169aaed1df993124d910a02004d7
Abdel Aziz Mfossa <abdelemfossa@gmail.com>
2026-03-11 00:27:05 +0100

    Refactor: Create base template and partials, fix portfolio URLs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index.html', views.home, name='home_html'),
-    path('projects.html', views.projects, name='projects'),
-    path('blog.html', views.blog, name='blog'),
-    path('events.html', views.events, name='events'),
-    path('videos.html', views.videos, name='videos'),
+    path('projects/', views.projects, name='projects'),
+    path('blog/', views.blog, name='blog'),
+    path('events/', views.events, name='events'),
+    path('videos/', views.videos, name='videos'),
 ]
