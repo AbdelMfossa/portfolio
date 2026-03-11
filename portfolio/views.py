1722b61b9f1bdcf0ef4d08d19c149cac46399fa1
Abdel Aziz Mfossa <abdelemfossa@gmail.com>
2026-03-11 02:12:44 +0100

    feat: render dynamic content on index, events and videos pages, fix git tracking

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,16 +1,43 @@
 from django.shortcuts import render
+from .models import Project, Article, Event, Video
 
 def home(request):
-    return render(request, 'portfolio/index.html')
+    projects = Project.objects.filter(status='production')[:2]
+    articles = Article.objects.all()[:3]
+    recent_videos = Video.objects.all()[:3]
+    events = Event.objects.all()[:3]
+    
+    return render(request, 'portfolio/index.html', {
+        'projects': projects,
+        'articles': articles,
+        'videos': recent_videos,
+        'events': events,
+    })
 
 def projects(request):
-    return render(request, 'portfolio/projects.html')
+    projects_list = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects': projects_list})
 
 def blog(request):
-    return render(request, 'portfolio/blog.html')
+    articles_list = Article.objects.all()
+    return render(request, 'portfolio/blog.html', {'articles': articles_list})
 
 def events(request):
-    return render(request, 'portfolio/events.html')
+    events_list = Event.objects.all()
+    return render(request, 'portfolio/events.html', {'events': events_list})
 
 def videos(request):
-    return render(request, 'portfolio/videos.html')
+    featured_video = Video.objects.filter(is_featured=True).first()
+    
+    if featured_video:
+        videos_list = Video.objects.exclude(id=featured_video.id)
+    else:
+        videos_list = Video.objects.all()
+        featured_video = videos_list.first() if videos_list.exists() else None
+        if featured_video:
+            videos_list = videos_list.exclude(id=featured_video.id)
+            
+    return render(request, 'portfolio/videos.html', {
+        'featured_video': featured_video,
+        'videos': videos_list
+    })
